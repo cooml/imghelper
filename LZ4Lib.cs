b4aae074eb0d64372d4c2523b63cc83bd70bc603
luoys4 <luoys4@LUOYS4-PC0WPAK1>
2021-06-23 11:21:00 +0800

    add file

---
+++ LZ4Lib.cs
@@ -0,0 +1,41 @@
+﻿using LZ4;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace imgtobase64
+{
+  /// <summary>
+  /// LZ4 压缩工具类
+  /// </summary>
+  public class LZ4Lib
+  {
+    /// <summary>
+    /// 压缩文本
+    /// </summary>
+    /// <param name="text">文本内容</param>
+    /// <returns></returns>
+    public static string CompressBuffer(string text)
+    {
+      var compressed = Convert.ToBase64String(
+          LZ4Codec.Wrap(Encoding.UTF8.GetBytes(text)));
+
+      return compressed;
+    }
+
+    /// <summary>
+    /// 解压文本
+    /// </summary>
+    /// <param name="compressed">压缩的文本</param>
+    /// <returns></returns>
+    public static string DecompressBuffer(string compressed)
+    {
+      var lorems =
+          Encoding.UTF8.GetString(
+              LZ4Codec.Unwrap(Convert.FromBase64String(compressed)));
+
+
+      return lorems;
+    }
+  }
+}
---
+++ SentEmil.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace imgtobase64
+{
+  public class SentEmil
+  {
+
+    public static string fnSendMailLive(string StrTo, string StrBody, string strSubjec)
+    {
+
+      System.Net.Mail.MailMessage onemail = new System.Net.Mail.MailMessage();
+
+      string myEmail = "a@onezl.com"; //发送邮件t的?邮箱?地?址
+
+      string myPwd = "123"; //
+
+      onemail.BodyEncoding = System.Text.Encoding.UTF8;
+
+      onemail.IsBodyHtml = true;
+
+
+      onemail.From = new System.Net.Mail.MailAddress(myEmail);
+      string[] ss = StrTo.Split(';');
+      for (int i = 0; i < ss.Length; i++)
+      {
+        onemail.To.Add(new System.Net.Mail.MailAddress(ss[i]));
+      }
+
+      onemail.Subject = strSubjec;
+
+      onemail.Body = StrBody;
+
+      System.Net.Mail.SmtpClient clint = new System.Net.Mail.SmtpClient("smtp.mxhichina.com", 25);//发送邮件t的?服t务?器
+
+      clint.Credentials = new System.Net.NetworkCredential(myEmail, myPwd);
+
+      clint.EnableSsl = true;
+
+      clint.Timeout = 15000;
+
+      try { clint.Send(onemail); }
+      catch (Exception ex) { return ex.Message; }
+
+      return "发送成功!";
+
+    }
+
+    public static string fnSendMailQQ(string StrTo, string StrBody, string strSubjec)
+    {
+
+      System.Net.Mail.MailMessage onemail = new System.Net.Mail.MailMessage();
+
+      string myEmail = "764607205@qq.com"; //发送邮件t的?邮箱?地?址
+
+      string myPwd = "000"; //发送邮件t的?邮箱?密码?
+
+      onemail.BodyEncoding = System.Text.Encoding.UTF8;
+
+      onemail.IsBodyHtml = true;
+
+
+      onemail.From = new System.Net.Mail.MailAddress(myEmail);
+      string[] ss = StrTo.Split(';');
+      for (int i = 0; i < ss.Length; i++)
+      {
+        onemail.To.Add(new System.Net.Mail.MailAddress(ss[i]));
+      }
+
+      onemail.Subject = strSubjec;
+
+      onemail.Body = StrBody;
+
+      System.Net.Mail.SmtpClient clint = new System.Net.Mail.SmtpClient("smtp.qq.com", 25);//发送邮件t的?服t务?器
+
+      clint.Credentials = new System.Net.NetworkCredential(myEmail, myPwd);
+
+      clint.EnableSsl = true;
+
+      clint.Timeout = 15000;
+
+      try { clint.Send(onemail); }
+      catch (Exception ex) { return ex.Message; }
+
+      return "发送成功!";
+
+    }
+
+  }
+}
